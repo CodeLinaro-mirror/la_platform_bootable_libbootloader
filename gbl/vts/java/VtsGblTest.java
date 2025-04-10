b53a7848bdae4badd5f6aecea017ea92ea795860
Yi-Yo Chiang <yochiang@google.com>
2025-04-10 17:56:10 +0800

    VtsGblTest: verify GBL certificate
    Verify that the GBL EFI application is not tempered with by varifying
    the embedded hash (in the GBL certificate) in the GBL EFI application.
    
    (cherry picked from aosp/41bf4821bde760f791b8c2568663e91dd8ab4d02)
    
    Bug: 387241522
    Test: gblsigntool sign gbl_x86_64.efi -o gbl_x64.efi.signed
    Test: launch_cvd --android_efi_loader=gbl_x64.efi.signed
    Test: atest VtsGblTest
    Merged-In: I1474a77625c4dea63a6df516b87ce2b832c6e1d1
    Change-Id: I1474a77625c4dea63a6df516b87ce2b832c6e1d1

--- gbl/vts/java/VtsGblTest.java
+++ gbl/vts/java/VtsGblTest.java
@@ -18,27 +18,45 @@ package com.android.tests.gbl;
 
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.Matchers.matchesPattern;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeThat;
 
+import com.android.compatibility.common.tradefed.build.CompatibilityBuildHelper;
 import com.android.tradefed.device.DeviceNotAvailableException;
 import com.android.tradefed.device.ITestDevice;
 import com.android.tradefed.log.LogUtil.CLog;
 import com.android.tradefed.testtype.DeviceJUnit4ClassRunner;
 import com.android.tradefed.testtype.junit4.BaseHostJUnit4Test;
+import com.android.tradefed.util.CommandResult;
+import com.android.tradefed.util.CommandStatus;
+import com.android.tradefed.util.FileUtil;
+import com.android.tradefed.util.RunUtil;
+import java.io.File;
 import java.io.IOException;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(DeviceJUnit4ClassRunner.class)
 public class VtsGblTest extends BaseHostJUnit4Test {
+  private File mTempDir;
+
   @Before
   public final void setUp() throws DeviceNotAvailableException, IOException {
     ITestDevice device = getDevice();
     final long gblVersion = device.getIntProperty("ro.boot.gbl.version", -1L);
     assumeThat("GBL version prop", gblVersion, not(-1L));
+
+    mTempDir = FileUtil.createTempDir("VtsGblTest");
+  }
+
+  @After
+  public final void tearDown() {
+    FileUtil.recursiveDelete(mTempDir);
   }
 
   @Test
@@ -63,5 +81,52 @@ public class VtsGblTest extends BaseHostJUnit4Test {
       return;
     }
     final long gblBuildIncremental = Long.parseLong(gblBuildNumber);
+  }
+
+  @Test
+  public void testCertificate() throws DeviceNotAvailableException, IOException {
+    ITestDevice device = getDevice();
+    File android_esp = new File(mTempDir, "android_esp");
+    assertTrue(
+        "Fetch android_esp partition",
+        device.pullFile("/dev/block/by-name/android_esp", android_esp));
+
+    CompatibilityBuildHelper buildHelper = new CompatibilityBuildHelper(getBuild());
+    File mtools = buildHelper.getTestFile("mtools");
+    File boot_efi = new File(mTempDir, "boot.efi");
+    boolean found_efi = false;
+    for (String efi_name :
+        new String[] {
+          "::/EFI/BOOT/BOOTAA64.EFI", "::/EFI/BOOT/BOOTX64.EFI", "::/EFI/BOOT/BOOTIA32.EFI"
+        }) {
+      boot_efi.delete();
+      CommandResult result =
+          new RunUtil()
+              .runTimedCmd(
+                  3000,
+                  mtools.getAbsolutePath(),
+                  "-c",
+                  "mcopy",
+                  "-i",
+                  android_esp.getAbsolutePath(),
+                  "-n",  // Don't complain about overwrite
+                  efi_name,
+                  boot_efi.getAbsolutePath());
+      if (CommandStatus.SUCCESS.equals(result.getStatus())) {
+        CLog.i("Found EFI application: " + efi_name);
+        found_efi = true;
+        break;
+      }
+    }
+    assertTrue("Found EFI application", found_efi);
+
+    // FIXME(b/387241522): Check public key hash.
+    File gblsigntool = buildHelper.getTestFile("gblsigntool");
+    CommandResult result =
+        new RunUtil()
+            .runTimedCmd(5000, gblsigntool.getAbsolutePath(), "verify", boot_efi.getAbsolutePath());
+    CLog.i("gblsigntool stdout: " + result.getStdout());
+    assertEquals(
+        "gblsigntool stderr: " + result.getStderr(), CommandStatus.SUCCESS, result.getStatus());
   }
 }
