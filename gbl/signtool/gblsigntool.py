e89e511ee54ededb047169ee4a5ff333594fc07d
Yi-Yo Chiang <yochiang@google.com>
2025-04-10 17:37:54 +0800

    signtool: Add gbl sign and verify commands
    Bug: 387241522
    Test: openssl genrsa 4096 > rsa.key
    Test: openssl rsa -in rsa.key -pubout > rsa.pub.key
    Test: m gblsigntool
    Test: gblsigntool sign gbl.efi -o gbl.efi.signed --avbtool_args '--key rsa.key --algorithm SHA512_RSA4096'
    Test: gblsigntool verify gbl.efi.signed --key rsa.pub.key
    Test: SecureBoot signature must not invalidate GBL certificate
    Test: sbsign gbl.efi.signed && gblsigntool verify gbl.efi.signed.signed
    Change-Id: I358ba861420d75dc6de336a28570a6b2bd1e2c97

---
+++ gbl/signtool/gblsigntool.py
@@ -0,0 +1,374 @@
+#!/usr/bin/env python3
+#
+# Copyright 2025, The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""Signs GBL images."""
+
+from argparse import ArgumentParser
+import hashlib
+import os
+import shlex
+import shutil
+import struct
+import tempfile
+
+from avbtool import (AvbFooter, AvbTool)
+
+
+# Source of truth is winnt.h
+IMAGE_DOS_SIGNATURE = 0x5A4D
+
+IMAGE_NT_SIGNATURE = 0x4550
+IMAGE_NT_OPTIONAL_HDR32_MAGIC = 0x010B
+IMAGE_NT_OPTIONAL_HDR64_MAGIC = 0x020B
+
+SIZEOF_IMAGE_DOS_HEADER = 64
+SIZEOF_IMAGE_PE_HEADER = 24
+
+OFFSET_E_LFANEW = 0x3C
+
+OFFSET_PE_NUMBER_OF_SECTIONS = 6
+OFFSET_PE_SIZE_OF_OPTIONAL_HEADER = 20
+
+OFFSET_OPTIONAL_HDR_SIZE_OF_HEADERS = 60
+OFFSET_OPTIONAL_HDR_CHECKSUM = 64
+OFFSET_OPTIONAL_HDR32_NUMBER_OF_RVA_AND_SIZES = 92
+OFFSET_OPTIONAL_HDR32_DATA_DIRECTORY = 96
+OFFSET_OPTIONAL_HDR64_NUMBER_OF_RVA_AND_SIZES = 108
+OFFSET_OPTIONAL_HDR64_DATA_DIRECTORY = 112
+
+OFFSET_SECTION_HEADER_SIZE_OF_RAW_DATA = 16
+OFFSET_SECTION_HEADER_POINTER_TO_RAW_DATA = 20
+
+CERTIFICATE_TABLE_IDX = 4
+
+
+def unpack_word(buf, off):
+  """Unpacks a little-endian 2-byte word."""
+  return struct.unpack_from('<H', buf, off)[0]
+
+
+def unpack_int(buf, off):
+  """Unpacks a little-endian 4-byte int."""
+  return struct.unpack_from('<I', buf, off)[0]
+
+
+class PEError(ValueError):
+  """PE file parsing related errors."""
+
+
+class DOSHeader:
+  """Helper class for DOS header."""
+
+  def __init__(self, buf):
+    self._buf = buf
+
+    if len(self._buf) < SIZEOF_IMAGE_DOS_HEADER:
+      raise PEError('Image size is too small for a DOS header')
+
+    magic = unpack_word(self._buf, 0)
+    if magic != IMAGE_DOS_SIGNATURE:
+      raise PEError(f'Unexpected DOS magic: 0x{magic:04X}')
+
+  def e_lfanew(self):
+    return unpack_word(self._buf, OFFSET_E_LFANEW)
+
+
+class PEHeader:
+  """Helper class for PE header."""
+
+  def __init__(self, buf):
+    self._buf = buf
+
+    if len(self._buf) < SIZEOF_IMAGE_PE_HEADER:
+      raise PEError('Image size is too small for a PE header')
+
+    magic = unpack_int(self._buf, 0)
+    if magic != IMAGE_NT_SIGNATURE:
+      raise PEError(f'Unexpected PE magic: 0x{magic:08X}')
+
+  def size_of_optional_header(self):
+    return unpack_word(self._buf, OFFSET_PE_SIZE_OF_OPTIONAL_HEADER)
+
+  def number_of_sections(self):
+    return unpack_word(self._buf, OFFSET_PE_NUMBER_OF_SECTIONS)
+
+
+class OptionalHeader:
+  """Helper class for PE32/PE32+ Optional header."""
+
+  def __init__(self, buf):
+    self._buf = buf
+
+    magic = unpack_word(self._buf, 0)
+    if magic == IMAGE_NT_OPTIONAL_HDR32_MAGIC:
+      self._pe_plus = False
+    elif magic == IMAGE_NT_OPTIONAL_HDR64_MAGIC:
+      self._pe_plus = True
+    else:
+      raise PEError(f'Unexpected PE Optional header magic: 0x{magic:04X}')
+
+  def number_of_data_entries(self):
+    if self._pe_plus:
+      offset = OFFSET_OPTIONAL_HDR64_NUMBER_OF_RVA_AND_SIZES
+    else:
+      offset = OFFSET_OPTIONAL_HDR32_NUMBER_OF_RVA_AND_SIZES
+    return unpack_int(self._buf, offset)
+
+  def data_entry_offset(self, idx):
+    if self._pe_plus:
+      return OFFSET_OPTIONAL_HDR64_DATA_DIRECTORY + idx * 8
+    else:
+      return OFFSET_OPTIONAL_HDR32_DATA_DIRECTORY + idx * 8
+
+  def data_entry(self, idx):
+    return struct.unpack_from('<II', self._buf, self.data_entry_offset(idx))
+
+  def size_of_headers(self):
+    return unpack_int(self._buf, OFFSET_OPTIONAL_HDR_SIZE_OF_HEADERS)
+
+
+class PEImage:
+  """PE file parser."""
+
+  def __init__(self, buf):
+    self._buf = bytearray(buf)
+
+    self._dos_header = DOSHeader(self._buf)
+    self._pe_header_offset = self._dos_header.e_lfanew()
+    self._optional_header_offset = (
+        self._pe_header_offset + SIZEOF_IMAGE_PE_HEADER
+    )
+    self._pe_header = PEHeader(self._buf[self._pe_header_offset :])
+    self._checksum_offset = (
+        self._optional_header_offset + OFFSET_OPTIONAL_HDR_CHECKSUM
+    )
+    self._section_headers_offset = (
+        self._optional_header_offset + self._pe_header.size_of_optional_header()
+    )
+    self._optional_header = OptionalHeader(
+        self._buf[self._optional_header_offset :]
+    )
+    if self._optional_header.number_of_data_entries() < 5:
+      raise PEError('PE Optional header data directories table is too small')
+
+  def erase_existing_win_certificates(self):
+    offset, size = self._optional_header.data_entry(CERTIFICATE_TABLE_IDX)
+    certificate_table_offset = (
+        self._optional_header_offset
+        + self._optional_header.data_entry_offset(CERTIFICATE_TABLE_IDX)
+    )
+    self._buf[certificate_table_offset : certificate_table_offset + 8] = (
+        8 * b'\x00'
+    )
+    if offset or size:
+      if offset > len(self._buf):
+        print('WARNING: certificate table offset is past EOF')
+        return
+      if offset + size != len(self._buf):
+        print('WARNING: removed junk data after the certificate table')
+      self._buf = self._buf[:offset]
+      print('Erased existing SecureBoot certificates')
+
+  def erase_checksum(self):
+    self._buf[self._checksum_offset : self._checksum_offset + 4] = 4 * b'\x00'
+
+  def get_avb_footer(self):
+    if len(self._buf) >= AvbFooter.SIZE:
+      try:
+        return AvbFooter(self._buf[-AvbFooter.SIZE :])
+      except (LookupError, struct.error):
+        pass
+    return None
+
+  def authenticode_digest(self):
+    data_directory_certificate_table_offset = (
+        self._optional_header_offset
+        + self._optional_header.data_entry_offset(CERTIFICATE_TABLE_IDX)
+    )
+    data_directory_certificate_table_end = (
+        self._optional_header_offset
+        + self._optional_header.data_entry_offset(CERTIFICATE_TABLE_IDX + 1)
+    )
+    regions = [
+        (0, self._checksum_offset),
+        (self._checksum_offset + 4, data_directory_certificate_table_offset),
+        (
+            data_directory_certificate_table_end,
+            self._optional_header.size_of_headers(),
+        ),
+    ]
+    for idx in range(self._pe_header.number_of_sections()):
+      off = self._section_headers_offset + idx * 40
+      size = unpack_int(self._buf, off + OFFSET_SECTION_HEADER_SIZE_OF_RAW_DATA)
+      data = unpack_int(
+          self._buf, off + OFFSET_SECTION_HEADER_POINTER_TO_RAW_DATA
+      )
+      regions.append((data, data + size))
+    regions.sort(key=lambda e: e[0])
+
+    # End junk
+    regions.append((regions[-1][1], len(self._buf)))
+
+    hasher = hashlib.sha256()
+    for begin, end in regions:
+      hasher.update(self._buf[begin:end])
+    return hasher.hexdigest()
+
+
+def gbl_info(args):
+  """Shows info about a GBL image."""
+  with open(args.gbl_image, 'rb') as gbl:
+    gbl_bytes = gbl.read()
+  gbl_image = PEImage(gbl_bytes)
+  gbl_image.erase_existing_win_certificates()
+  gbl_image.erase_checksum()
+  print('Authenticode digest (sha256):', gbl_image.authenticode_digest())
+
+  avb_footer = gbl_image.get_avb_footer()
+  if not avb_footer:
+    print('AVB footer not found, image is unsigned')
+    return
+
+  with tempfile.TemporaryDirectory() as temp_dir:
+    gbl_efi = os.path.join(temp_dir, 'gbl.efi')
+    with open(gbl_efi, 'wb') as f:
+      f.write(gbl_image._buf)
+    gbl_image._buf = gbl_image._buf[: avb_footer.original_image_size]
+    print(
+        'Authenticode digest (without AVB footer):',
+        gbl_image.authenticode_digest(),
+    )
+    print('====VBMETA====')
+    AvbTool().run(['avbtool', 'info_image', '--image', gbl_efi])
+
+
+def gbl_sign(args):
+  """Signs a GBL image."""
+  with open(args.gbl_image, 'rb') as gbl:
+    gbl_bytes = gbl.read()
+  gbl_image = PEImage(gbl_bytes)
+  gbl_image.erase_existing_win_certificates()
+  gbl_image.erase_checksum()
+  avb_footer = gbl_image.get_avb_footer()
+  if avb_footer:
+    gbl_image._buf = gbl_image._buf[: avb_footer.original_image_size]
+    print('Erased existing AVB footer')
+  digest = gbl_image.authenticode_digest()
+
+  with tempfile.TemporaryDirectory() as temp_dir:
+    gbl_efi = os.path.join(temp_dir, 'gbl.efi')
+    with open(gbl_efi, 'wb') as f:
+      f.write(gbl_image._buf)
+    avb_cmd = (
+        [
+            'avbtool',
+            'add_hash_footer',
+        ]
+        + args.avbtool_args
+        + [
+            '--image',
+            gbl_efi,
+            '--partition_name',
+            'gbl',
+            '--dynamic_partition_size',
+            '--prop',
+            f'authenticode:{digest}',
+        ]
+    )
+    print('avbtool command:', ' '.join(avb_cmd))
+    AvbTool().run(avb_cmd)
+    print('Authenticode digest (sha256):', digest)
+    shutil.copy2(gbl_efi, args.output)
+    print(f'Signed image written to {args.output}')
+
+
+def gbl_verify(args):
+  """Verifies a signed GBL image."""
+  with open(args.gbl_image, 'rb') as gbl:
+    gbl_bytes = gbl.read()
+  gbl_image = PEImage(gbl_bytes)
+  gbl_image.erase_existing_win_certificates()
+  gbl_image.erase_checksum()
+
+  with tempfile.TemporaryDirectory() as temp_dir:
+    gbl_efi = os.path.join(temp_dir, 'gbl.efi')
+    with open(gbl_efi, 'wb') as f:
+      f.write(gbl_image._buf)
+    avb_cmd = [
+        'avbtool',
+        'verify_image',
+        '--image',
+        gbl_efi,
+    ]
+    if args.key:
+      avb_cmd += ['--key', args.key]
+    AvbTool().run(avb_cmd)
+
+
+def flatten_args(raw_args):
+  """Split and flatten nested args."""
+  args = []
+  for a in raw_args:
+    args.extend(shlex.split(a))
+  return args
+
+
+def main():
+  parser = ArgumentParser()
+  subcommands = parser.add_subparsers(required=True, title='subcommands')
+
+  info_command = subcommands.add_parser(
+      'info', help='show info about a GBL image'
+  )
+  info_command.add_argument(
+      'gbl_image', metavar='GBL_IMAGE', help='GBL EFI image'
+  )
+  info_command.set_defaults(func=gbl_info)
+
+  sign_command = subcommands.add_parser('sign', help='sign a GBL image')
+  sign_command.add_argument(
+      'gbl_image', metavar='GBL_IMAGE', help='GBL EFI image'
+  )
+  sign_command.add_argument(
+      '-o', '--output', required=True, help='output file name'
+  )
+  sign_command.add_argument(
+      '--avbtool_args',
+      default=[],
+      action='append',
+      help='signing args to pass to avbtool (can be specified multiple times)',
+  )
+  sign_command.set_defaults(func=gbl_sign)
+
+  verify_command = subcommands.add_parser(
+      'verify', help='verify a signed GBL image'
+  )
+  verify_command.add_argument(
+      'gbl_image', metavar='GBL_IMAGE', help='GBL EFI image'
+  )
+  verify_command.add_argument('--key', help='check public key')
+  verify_command.set_defaults(func=gbl_verify)
+
+  args = parser.parse_args()
+  if 'avbtool_args' in args:
+    args.avbtool_args = flatten_args(args.avbtool_args)
+
+  return args.func(args)
+
+
+if __name__ == '__main__':
+  main()
