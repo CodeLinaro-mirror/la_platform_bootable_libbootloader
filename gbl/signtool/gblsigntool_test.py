cec49dd0269ec293df62853672f9be278d06a523
Yi-Yo Chiang <yochiang@google.com>
2025-05-14 18:30:32 +0800

    gblsigntool: Add 'remove' command and improve test coverage
    Test: ./tools/bazel test @gbl//signtool:gblsigntool_test
    Change-Id: If23b80ca3ddea45d69e12126a70dd29269db50fd

--- gbl/signtool/gblsigntool_test.py
+++ gbl/signtool/gblsigntool_test.py
@@ -14,88 +14,133 @@
 # limitations under the License.
 #
 
-import glob
-import os
+
+from pathlib import Path
 import shutil
 import subprocess
-import sys
 import tempfile
 import unittest
 
-EXEC_DIR = os.path.abspath(os.path.dirname(sys.argv[0]))
+
+def GetResource(*args):
+  return Path(__file__).absolute().parent.joinpath(*args)
 
 
-def get_resource(*args):
-  return os.path.join(EXEC_DIR, *args)
+def Gblsigntool(*args):
+  return subprocess.run([GetResource('gblsigntool')] + list(args), check=True)
+
+
+TEST_KEY_PATH = GetResource('testdata', 'testkey_RSA4096.pem')
+TEST_PUBKEY_PATH = GetResource('testdata', 'testkey_RSA4096_pub.pem')
+
+# These are originally testkey-signed by another key, _not_ the above key.
+TEST_EFI_PATH = GetResource('testdata', 'gbl_aarch64_prod.efi')
+TEST_IMG_ZIP_PATH = GetResource('testdata', 'gbl-img-13454081.zip')
 
 
 class GblSigntoolTest(unittest.TestCase):
 
-  def setUp(self):
-    self._gblsigntool = get_resource('gblsigntool')
-    self._test_key = get_resource('testkeys', 'testkey_RSA4096.pem')
-    self._test_pubkey = get_resource('testkeys', 'testkey_RSA4096_pub.pem')
-    self._test_efi = get_resource('testdata', 'gbl_aarch64_prod.efi')
-    self._test_img_zip = get_resource('testdata', 'gbl-img.zip')
+  def testInfo_success(self):
+    Gblsigntool('info', TEST_EFI_PATH)
 
-  def testVerify_withUnsignedInput(self):
-    result = subprocess.run([
-        self._gblsigntool,
-        'verify',
-        self._test_efi,
-        '--key',
-        self._test_pubkey,
-    ])
-    self.assertNotEqual(result.returncode, 0)
-
-  def testSignAndVerify(self):
+  def testInfo_notPEImage_failure(self):
     with tempfile.TemporaryDirectory() as temp_dir:
-      signed_efi = os.path.join(temp_dir, 'signed.img')
-      subprocess.run(
-          [
-              self._gblsigntool,
-              'sign',
-              self._test_efi,
-              '-o',
-              signed_efi,
-              '--avbtool_args',
-              f'--key "{self._test_key}" --algorithm SHA256_RSA4096',
-          ],
-          check=True,
-      )
-      subprocess.run(
-          [self._gblsigntool, 'verify', signed_efi, '--key', self._test_pubkey],
-          check=True,
-      )
+      regular_file = Path(temp_dir).joinpath('regular_file')
+      with open(regular_file, 'wb') as f:
+        f.write(b'blah' * 1000)
+      with self.assertRaises(subprocess.CalledProcessError):
+        Gblsigntool('info', regular_file)
 
-  def testSignArchiveAndVerify(self):
+  def testInfo_unsigned_failure(self):
     with tempfile.TemporaryDirectory() as temp_dir:
-      signed_zip = os.path.join(temp_dir, 'signed.zip')
-      subprocess.run(
-          [
-              self._gblsigntool,
-              'sign_archive',
-              self._test_img_zip,
-              '-o',
-              signed_zip,
-              '--avbtool_args',
-              f'--key "{self._test_key}" --algorithm SHA256_RSA4096',
-          ],
-          check=True,
+      unsigned_efi = Path(temp_dir).joinpath('unsigned.efi')
+      Gblsigntool('remove', TEST_EFI_PATH, '-o', unsigned_efi)
+      with self.assertRaises(subprocess.CalledProcessError):
+        Gblsigntool('info', unsigned_efi)
+
+  def testRemove_idempotent(self):
+    with tempfile.TemporaryDirectory() as temp_dir:
+      unsigned_efi = Path(temp_dir).joinpath('unsigned.efi')
+      Gblsigntool('remove', TEST_EFI_PATH, '-o', unsigned_efi)
+      unsigned_twice_efi = Path(temp_dir).joinpath('unsigned_twice.efi')
+      Gblsigntool('remove', unsigned_efi, '-o', unsigned_twice_efi)
+      with open(unsigned_efi, 'rb') as f:
+        unsigned_bytes = f.read()
+      with open(unsigned_twice_efi, 'rb') as f:
+        unsigned_twice_bytes = f.read()
+      self.assertEqual(unsigned_bytes, unsigned_twice_bytes)
+
+  def testVerify_success(self):
+    Gblsigntool('verify', TEST_EFI_PATH)
+
+  def testVerify_pubkeyMismatch_failure(self):
+    with self.assertRaises(subprocess.CalledProcessError):
+      Gblsigntool('verify', TEST_EFI_PATH, '--key', TEST_PUBKEY_PATH)
+
+  def testVerify_notPEImage_failure(self):
+    with tempfile.TemporaryDirectory() as temp_dir:
+      regular_file = Path(temp_dir).joinpath('regular_file')
+      with open(regular_file, 'wb') as f:
+        f.write(b'blah' * 1000)
+      with self.assertRaises(subprocess.CalledProcessError):
+        Gblsigntool('verify', regular_file)
+
+  def testVerify_unsigned_failure(self):
+    with tempfile.TemporaryDirectory() as temp_dir:
+      unsigned_efi = Path(temp_dir).joinpath('unsigned.efi')
+      Gblsigntool('remove', TEST_EFI_PATH, '-o', unsigned_efi)
+      with self.assertRaises(subprocess.CalledProcessError):
+        Gblsigntool('verify', unsigned_efi)
+
+  def testSign_success(self):
+    with tempfile.TemporaryDirectory() as temp_dir:
+      unsigned_efi = Path(temp_dir).joinpath('unsigned.efi')
+      Gblsigntool('remove', TEST_EFI_PATH, '-o', unsigned_efi)
+      signed_efi = Path(temp_dir).joinpath('signed.efi')
+      Gblsigntool(
+          'sign',
+          unsigned_efi,
+          '-o',
+          signed_efi,
+          '--avbtool_args',
+          f'--key "{TEST_KEY_PATH}" --algorithm SHA256_RSA4096',
       )
-      unpack_dir = os.path.join(temp_dir, 'signed')
-      shutil.unpack_archive(signed_zip, unpack_dir)
-      for signed_efi in glob.glob(os.path.join(unpack_dir, 'gbl*.efi')):
-        subprocess.run(
-            [
-                self._gblsigntool,
-                'verify',
-                signed_efi,
-                '--key',
-                self._test_pubkey,
-            ],
-            check=True,
-        )
+      Gblsigntool('verify', signed_efi, '--key', TEST_PUBKEY_PATH)
+
+  def testSign_resign_success(self):
+    with tempfile.TemporaryDirectory() as temp_dir:
+      signed_efi = Path(temp_dir).joinpath('signed.efi')
+      Gblsigntool(
+          'sign',
+          TEST_EFI_PATH,
+          '-o',
+          signed_efi,
+          '--avbtool_args',
+          f'--key "{TEST_KEY_PATH}" --algorithm SHA256_RSA4096',
+      )
+      Gblsigntool('verify', signed_efi, '--key', TEST_PUBKEY_PATH)
+
+  def testSignArchive_success(self):
+    with tempfile.TemporaryDirectory() as temp_dir:
+      unpack_dir = Path(temp_dir).joinpath('unpack')
+      shutil.unpack_archive(TEST_IMG_ZIP_PATH, unpack_dir)
+      for efi in unpack_dir.glob('gbl*.efi'):
+        with self.assertRaises(subprocess.CalledProcessError):
+          Gblsigntool('verify', efi, '--key', TEST_PUBKEY_PATH)
+
+      signed_zip = Path(temp_dir).joinpath('signed.zip')
+      Gblsigntool(
+          'sign_archive',
+          TEST_IMG_ZIP_PATH,
+          '-o',
+          signed_zip,
+          '--avbtool_args',
+          f'--key "{TEST_KEY_PATH}" --algorithm SHA256_RSA4096',
+      )
+      unpack_signed_dir = Path(temp_dir).joinpath('unpack_signed')
+      shutil.unpack_archive(signed_zip, unpack_signed_dir)
+      for signed_efi in unpack_signed_dir.glob('gbl*.efi'):
+        Gblsigntool('verify', signed_efi, '--key', TEST_PUBKEY_PATH)
 
 
 if __name__ == '__main__':
