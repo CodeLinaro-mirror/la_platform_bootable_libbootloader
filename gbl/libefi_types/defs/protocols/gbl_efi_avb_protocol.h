9dffd5cc01d8cb392b1b5af5da0034b98453b40b
Dmitrii Merkurev <dimorinny@google.com>
2025-06-04 16:14:22 +0000

    Allow GBL protocols to be used under BSD-2-Clause-Patents
    To upstream GBL protocols headers to EDK2 and other FW in
    the future
    
    Bug: 419440648
    Change-Id: I8c5f9a87349335a06ac8ad62d1d71d3f42dd1d79
    Signed-off-by: Dmitrii Merkurev <dimorinny@google.com>

--- gbl/libefi_types/defs/protocols/gbl_efi_avb_protocol.h
+++ gbl/libefi_types/defs/protocols/gbl_efi_avb_protocol.h
@@ -12,6 +12,15 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR BSD-2-Clause-Patent
+ *
+ * You may choose to use or redistribute this file under
+ *  (a) the Apache License, Version 2.0, or
+ *  (b) the BSD 2-Clause Patent license.
+ *
+ * Unless you expressly elect the BSD-2-Clause-Patent terms, the Apache-2.0
+ * terms apply by default.
  */
 
 // This is a custom protocol introduced by GBL.
